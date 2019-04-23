b3241eca177cf76fa6dbde80918f4e98d8fa4f83
Asim Mac <asim@Asims-MacBook-Pro.local>
2019-04-23 19:11:43 +0500

    First commit

---
+++ index.js
@@ -0,0 +1,53 @@
+//simple script of ai chatbot using dialog flow and web speech api.
+'use strict';
+
+//require('dotenv').config()
+const APIAI_TOKEN = '19003a33ef324f41bf69054fabc1aa98';
+const APIAI_SESSION_ID = 'b923775b991b48e19c526967146d8bf7';
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/views')); // html
+app.use(express.static(__dirname + '/public')); // js, css, images
+
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+const io = require('socket.io')(server);
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+const apiai = require('apiai')(APIAI_TOKEN);
+
+// Web UI
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
+io.on('connection', function(socket) {
+  socket.on('chat message', (text) => {
+    console.log('Message: ' + text);
+
+    // Get a reply from API.ai
+
+    let apiaiReq = apiai.textRequest(text, {
+      sessionId: APIAI_SESSION_ID
+    });
+
+    apiaiReq.on('response', (response) => {
+      let aiText = response.result.fulfillment.speech;
+      console.log('Bot reply: ' + aiText);
+      socket.emit('bot reply', aiText);
+    });
+
+    apiaiReq.on('error', (error) => {
+      console.log(error);
+    });
+
+    apiaiReq.end();
+
+  });
+});
